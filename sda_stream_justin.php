aca2b1278f15c6b17d1326dfc1f78e449a05f310
Ian Bennett <webdev@ianbennett.net>
2014-08-09 16:56:03 +0100

    Fix: Remove Justin API and create Twitch API

--- sda_stream_justin.php
+++ sda_stream_justin.php
@@ -1,85 +1,10 @@
 <?php
 
-require_once( dirname(__FILE__) .'/sda_stream.php' );
+// NOTE: The Justin API was removed in August 2014.
+// This placeholder uses the Twitch API to maintain compatibility with old Justin channels.
 
-class SDAStreamJustin extends SDAStream {
+require_once( dirname(__FILE__) .'/sda_stream_twitch.php' );
 
-  const ttl = 1;
-
-  protected static function fields() {
-    return array(
-      'user_name'     => "[channel][login]",
-      'user_url'      => "[channel][channel_url]",
-      'channel_id'    => "[channel][id]",
-      'channel_name'  => "[channel][login]",
-      'channel_title' => "[channel][title]",
-      'channel_url'   => "[channel][channel_url]",
-      'embed_stream'  => "[channel][embed_code]",
-      'description'   => "[channel][status]",
-      'online'        => "[online]",
-    );
-  }
+class SDAStreamJustin extends SDAStreamTwitch {
   
-  protected static function query($channels, $ttl = null) {
-    $requests = $sets = $results = $s_sets = $s_requests = array();
-    
-    // Create API URL(s) and make requests
-    $keys = array_chunk( parent::array_every($channels, 'channel'), 150, true );
-    foreach ($keys as $v) {
-      $requests[] = self::api_url( implode(',', $v) );
-    }
-    $responses = parent::query_api($requests);
-        
-    // Post-process the results
-    $out = $online = array();
-    $class = get_called_class();
-    foreach ($responses as $response) {
-      foreach ($response as $c) {
-        $out[] = $class::post_process($c);
-        $online[$c['channel']['login']] = true;
-      }
-    }
-    
-    // Recreate (or at least try to) the offline channels
-    // (Justin's stream search doesn't return offline channels)
-    foreach ($channels as $c) {
-      if (!isset($online[$c['channel']])) {
-        $out[] = $class::post_process(array(
-          'channel'  => array(
-            'login'       => $c['channel'],
-            'channel_url' => "http://www.justin.tv/{$c['channel']}",
-            'embed_code'  => self::embed_channel($c['channel']),
-          ),
-          'online'        => false,
-        ));
-      }
-    }
-
-    return $out;
-  }
-  
-  protected static function api_url($channels) {
-    return "http://api.justin.tv/api/stream/list.json?channel=$channels";
-  }
-  
-  protected static function post_process($c) {
-    if (!isset($c['online'])) $c['online'] = true;
-    $c['channel']['embed_code'] = self::embed_channel($c['channel']['login']);
-    return $c;
-  }
-
-  public static function embed_channel($c) {
-    return <<<HTML
-<object type="application/x-shockwave-flash" height="295" width="353" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=$c" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="start_volume=25&watermark_position=top_right&channel=$c&auto_play=false" /></object>
-HTML;
-  }
-
-  public static function embed_chat($c) {
-    return <<<HTML
-<iframe frameborder="0" scrolling="no" id="chat_embed" src="http://twitch.tv/chat/embed?channel=$c&amp;popout_chat=true" height="301" width="221"></iframe>
-HTML;
-
-  }
-
-
 }
--- sda_stream_twitch.php
+++ sda_stream_twitch.php
@@ -1,15 +1,96 @@
 <?php
 
-require_once( dirname(__FILE__) .'/sda_stream_justin.php' );
+require_once( dirname(__FILE__) .'/sda_stream.php' );
 
-class SDAStreamTwitch extends SDAStreamJustin {
+class SDAStreamTwitch extends SDAStream {
 
-  protected static function post_process($c) {
-    foreach ( array('user_url', 'channel_url', 'embed_code') as $k ) {
-      if (isset($c['channel'][$k]))
-        $c['channel'][$k] = str_replace('http://www.justin.tv/', "http://www.twitch.tv/", $c['channel'][$k]);
-    }
-    return parent::post_process($c);
+  const ttl = 1;
+  const limit = 100;
+
+  protected static function fields() {
+    return array(
+      'user_name'     => "[channel][name]",
+      'user_url'      => "[channel][url]",
+      'channel_id'    => "[channel][_id]",
+      'channel_name'  => "[channel][name]",
+      //'channel_title' => "[channel][title]",
+      'channel_url'   => "[channel][url]",
+      'embed_stream'  => "[channel][embed_code]",
+      'description'   => "[channel][status]",
+      'online'        => "[online]",
+    );
   }
+  
+  protected static function query($channels, $ttl = null) {
+    $requests = $sets = $results = $s_sets = $s_requests = array();
+    
+    // Create API URL and make first request
+    $keys = parent::array_every($channels, 'channel');
+    $url = array( self::api_url( implode(',', $keys) ) );
+    $responses = parent::query_api($url);
+    
+    // Get the number of results and (if necessary) create later requests
+    $total = $responses[0]['_total'];
+    if ( (is_int($total)) && ($total > self::limit) ) {
+      $requests = array();
+      $ct = ceil($total / self::limit);
+      for ($i = 1; $i <= $ct; $i++) {
+        $requests[] = $url . '&offset=' . ($i * self::limit);
+      }
+      $responses = array_merge($responses, parent::query_api($requests));
+    }
+        
+    // Post-process the results
+    $out = $online = array();
+    $class = get_called_class();
+    foreach ($responses as $response) {
+      foreach ($response['streams'] as $c) {
+        $out[] = $class::post_process($c);
+        $online[$c['channel']['name']] = $c['online'] = true;
+        
+      }
+    }
+    
+    // Recreate (or at least try to) the offline channels
+    // (Twitch's stream search doesn't return offline channels)
+    foreach ($channels as $c) {
+      if (!isset($online[$c['channel']])) {
+        $out[] = $class::post_process(array(
+          'channel'  => array(
+            'name'        => $c['channel'],
+            'url'         => "http://www.twitch.tv/{$c['channel']}",
+            'embed_code'  => self::embed_channel($c['channel']),
+          ),
+          'online'        => false,
+        ));
+      }
+    }
+
+    return $out;
+  }
+  
+  protected static function api_url($channels) {
+    return "https://api.twitch.tv/kraken/streams?channel=$channels&limit=" . self::limit;
+  }
+  
+  protected static function post_process($c) {
+    if (!isset($c['online'])) $c['online'] = true;
+    $c['channel']['embed_code'] = self::embed_channel($c['channel']['name']);
+    return $c;
+  }
+
+  public static function embed_channel($c) {
+    return <<<HTML
+<object type="application/x-shockwave-flash" height="295" width="353" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=$c" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="start_volume=25&hostname=www.twitch.tv&channel=$c&auto_play=false" /></object>
+HTML;
+  }
+
+  public static function embed_chat($c) {
+    return <<<HTML
+<iframe frameborder="0" scrolling="no" id="chat_embed" src="http://twitch.tv/chat/embed?channel=$c&amp;popout_chat=true" height="301" width="221"></iframe>
+HTML;
+
+  }
+
   
 }
